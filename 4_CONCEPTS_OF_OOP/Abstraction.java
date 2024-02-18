2fad4209d52ab44ca3aa9e577f606ab91879ec68
CARL JANUS <160302599+SIDJAYY@users.noreply.github.com>
2024-02-18 23:07:03 +0800

    Add files via upload

---
+++ 4_CONCEPTS_OF_OOP/Abstraction.java
@@ -0,0 +1,21 @@
+abstract class BasicAttacks{ // <- superclass
+  abstract void attack();
+}
+
+class Ling extends BasicAttacks{ // <- subclass
+
+  @override
+  void attack(){ // <- creating the body of attack method in superclass
+    System.out.println("normal damage,Critical damage");
+  }
+}
+
+
+public class Abstraction{
+  public static void main(String a[]){
+    Ling ling  = new Ling();
+    ling.attack();
+  }
+}
+//abstract = classes cannot be intantiated, but the can have subclass
+//           abstract method are declared without an implementation.
